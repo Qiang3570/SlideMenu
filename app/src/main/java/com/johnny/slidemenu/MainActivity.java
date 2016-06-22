3d82a20621f56a1f8e14579ae507e5733ace44aa
johnny <Qiang3570@126.com>
2016-06-22 21:18:11 +0800

    Initial commit

---
+++ app/src/main/java/com/johnny/slidemenu/MainActivity.java
@@ -0,0 +1,16 @@
+package com.johnny.slidemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.johnny.slidemenu.view.SlideMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
